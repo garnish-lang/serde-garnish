b39427111f90d759fba3f047ec76fb0588db4263
Chad Collins <chadrcollins91@gmail.com>
2022-10-30 10:04:47 -0400

    Added necessary files

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod deserializer;
+mod deserialize;
+mod serializer;
+mod serialize;
+mod visitor;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
