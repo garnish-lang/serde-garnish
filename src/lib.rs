6759148fda7dd50a4fc4522a2033b3d84723b8c3
Chad Collins <chadrcollins91@gmail.com>
2022-11-01 20:08:59 -0400

    Corrections to conversions table

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@ mod error;
 /// | 5.0               | Number            | f32, f64                              |
 /// | "a"               | Char              | char                                  |
 /// | "abcd"            | CharList          | String                                |
-/// | '1'               | Byte              | Vec<u8>                               |
+/// | '1'               | Byte              | u8                                    |
 /// | '1234'            | ByteList          | Vec<u8>                               |
 /// | #5                | Type              | Enum                                  |
 /// | ;symbol           | Symbol            | Enum                                  |
@@ -47,7 +47,7 @@ mod error;
 /// | 5..10             | Range             | ?                                     |
 /// | 5 <> 10           | Concatenation     | Vec<T>                                |
 /// | list ~ 1..3       | Slice             | Vec<T>                                |
-/// | 10, 20, 30        | List              | List, Map, Struct                     |
+/// | 10, 20, 30        | List              | List, Map, Struct, Tuple              |
 /// | { 5 + 5 }         | Expression        | ?                                     |
 /// | external_value    | External          | ?                                     |
 /// | custom_type       | Custom            | ?                                     |
